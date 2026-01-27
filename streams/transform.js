f8ef91a02b7ab7a6f405ab51abeed066029df991
Harendra Pratap <prataph506@gmail.com>
2026-01-27 15:57:45 +0530

    [Harendra Pratrap].add Added new file to Classroom_Practice branch

---
+++ streams/transform.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const { Transform } = require("stream");
+
+const inputFilePath = path.join(__dirname, "input.txt");
+const transformOutputFilePath = path.join(__dirname, "transformOutput.txt");
+
+const readStream = fs.createReadStream(inputFilePath, {encoding:"utf-8"});
+const writeStream = fs.createWriteStream(transformOutputFilePath);
+
+const upperCaseTransform = new Transform({
+    transform(chunk, encoding, callback){
+        const transformedData = chunk.toString().toUpperCase();
+        this.push(transformedData);
+        callback();
+    }
+})
+
+readStream.pipe(upperCaseTransform).pipe(writeStream);
